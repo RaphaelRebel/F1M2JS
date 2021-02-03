feae007d7697920cdf1ba69da3ca7eb9b56bd286
RaphaelRebel <44580169+RaphaelRebel@users.noreply.github.com>
2021-02-03 14:54:34 +0100

    Add files via upload
    Les 5 finished

---
+++ Les05/Quiz-Javascript/quiz1.js
@@ -0,0 +1,132 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://www.lamlisse.nl/wp-content/uploads/2018/03/LAM-Lisse-logo-white.svg"
+
+    },
+    "quizContent":		
+		[
+			{
+				// vraag 1
+				"question": "Wat voor museum is het LAM?",
+				"answers": [{
+						"answer": "Geschiedenis",
+						"feedback": false
+					},
+					{
+						"answer": "Foodart",
+						"feedback": true
+					},
+					{
+						"answer": "Natuur",
+						"feedback": false
+					},
+					
+				]
+			},
+			{
+				// vraag 2
+				"question": "Welk kunstwerk bevind zich in het LAM museum?",
+				"answers": [{
+						"answer": "De Nachtwacht",
+						"feedback": false
+					},
+					{
+						"answer": "De bad grapes",
+						"feedback": true
+					},
+					{
+						"answer": "de slag bij waterloo",
+						"feedback": false
+					},
+					
+				]
+			},
+			
+			{
+				// Vraag 3
+				"question": "Welk kasteel ligt in de buurt van het museum?",
+				"answers": [{
+						"answer": "Kasteel radboud",
+						"feedback": false
+					},
+					{
+						"answer": "Kasteel keukenhof",
+						"feedback": true
+					},
+					{
+						"answer": "Kasteel brederode",
+						"feedback": false
+					},
+					
+				]
+			},
+			{
+				// vraag 4
+				"question": "Waar ligt het LAM museum?",
+				"answers": [{
+						"answer": "Lisse",
+						"feedback": true
+					},
+					{
+						"answer": "Sassenheim",
+						"feedback": false
+					},
+					{
+						"answer": "Amesfoort",
+						"feedback": false
+					},
+
+					
+					
+				]
+				
+			},
+			{
+				// vraag 5
+				"question": "Voor welke leeftijd groep is het LAM",
+				"answers": [{
+						"answer": "Kinderen en jongeren",
+						"feedback": false
+					},
+					{
+						"answer": "Volwassenen",
+						"feedback": false
+					},
+					{
+						"answer": "Voor alle leeftijden",
+						"feedback": true
+					},
+					
+
+					
+					
+				]
+				
+			},
+			{
+				// vraag 6
+				"question": "Op welke dagen is het LAM open?",
+				"answers": [{
+						"answer": "Maandag t/m vrijdag",
+						"feedback": false
+					},
+					{
+						"answer": "Dinsdag t/m zaterdag",
+						"feedback": false
+					},
+					{
+						"answer": "Woensdag en vrijdag t/m zondag",
+						"feedback": true
+					},
+
+					
+					
+				]
+				
+			}
+		]
+	}
