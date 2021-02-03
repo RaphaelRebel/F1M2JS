d61c7439681dedfcc94100371e516e4bd8b4c106
RaphaelRebel <44580169+RaphaelRebel@users.noreply.github.com>
2021-02-03 14:54:02 +0100

    Add files via upload
    Les 4 finished

---
+++ Les04/ma-webxr/app.js
@@ -0,0 +1,42 @@
+// voeg controls toe (teleport & WASD controls)
+const controls = new Controls(50);
+controls.enableMovement();
+
+// create a model
+const minecraftModel = new Model('assets/models/minecraft/scene.gltf');
+minecraftModel.setPosition(0, 1.54, -5);
+minecraftModel.setScale(0.1, 0.1, 0.1);
+minecraftModel.setRotation(0, -36.95, 0);
+
+const cubeRoom = new Model('assets/models/cube-room/cube-room.gltf');
+cubeRoom.setPosition(0, 1.54, -5);
+cubeRoom.setScale(0.6, 0.6, 0.6);
+cubeRoom.setRotation(0, -36.95, 0);
+
+// create a primitive (sphere)
+const sphere = new Sphere();
+sphere.setColor('green');
+sphere.setRadius(0.5);
+sphere.setPosition(-3, 0, -5);
+
+// create some text
+const testText = new Text('Mediacollege Amsterdam WebXR');
+testText.setPosition(1.5, 0, -5);
+testText.setFontsize(10);
+
+// create an image
+const image = new XRImage('assets/images/sampleImage.jpg');
+image.setPosition(-1.48819, 2.45839, -5);
+
+const folkerDeJong = new XRImage('assets/images/FolkertdeJong.jpg');
+folkerDeJong.setPosition(-2.48819, 3.45839, -2);
+
+// create a 360 image
+const image360 = new Sky('assets/360_world.jpg');
+
+const spotLight = new SpotLight();
+spotLight.setPosition(-3.39382, 4.72931, 1.37905);
+spotLight.setTarget(minecraftModel);
+
+// listen for an event
+minecraftModel.addEventListener('click', () => console.log('je klikt op mij'));
